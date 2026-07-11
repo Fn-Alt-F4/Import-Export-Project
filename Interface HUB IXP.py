70ab6ca572dbc2dc16d6893fe449a0c4002ece84
Evan <evantheboss200@gmail.com>
2026-07-10 22:17:35 -0400

    Add files via upload

---
+++ Interface HUB IXP.py
@@ -0,0 +1,21 @@
+
+
+#Now we are creating the interface for the HUB
+
+interface = {
+    "employees" : {"John Hancock" : "Driver" , "Sam Darnold" : "Packer" , "Bernie Dugg" : "Driver" , "Sarah Josk" : "Front Desk" , "Donald Maskell" : "Head of Export" , 
+                   "Paul Pogba" : "CFO" , "Dick Cheney" : "CEO"} ,
+    
+    "payroll" : {"John Hancock" : "145000 Anually" , "Sam Darnold" : "89000 Anually" , "Bernie Dugg" : "123000 Anually" , "Sarah Josk" : "54000 Anually" , "Donald Maskell" : "235000 Anually" ,
+                 "Paul Pogba" : "374000 Anually" , "Dick Cheney" : "483000 Anually"} ,
+    
+    "imports" : {"Solo Cups" : {"shipping to San Antonio Texas" : "40000 Total cups"} , "Paper Plates" : {"Shipping to Clevland Ohio" : "80000 Total Plates"} , 
+                 "Plastic Forks" : {"Shipping to North Pole Alaska" : "430000 Total Forks" } , "Plastic Spoons" : {"Shipping to Las Vegas Nevada" : "430000 Total Spoons"} ,
+                 "Plastic Kives" : {"Shipping to Charlotte North Carolina" : "390000 Total Knives"} , "Tissues" : {"Shipping to Philidelphia Penselvania" : "4000 Total Boxes"}} ,
+
+    "Exports" : {"Solo Cups" : {"Shipping to Miami Florida" : "52000 Total Cups"} , "Paper Plates" : {"Shipping to Denver Colorado" : "67000 Total Plates"} ,
+                 "Plastic Forks" : {"Shipping to Portland Oregon" : "310000 Total Forks"} , "Plastic Spoons" : {"Shipping to Nashville Tennessee" : "285000 Total Spoons"} ,
+                 "Plastic Knives" : {"Shipping to Austin Texas" : "295000 Total Knives"} , "Tissues" : {"Shipping to Boston Massachusetts" : "6200 Total Boxes"}},
+
+    "Settings" : {"Quit" : "Logout" , "Display Settings" : "Resolution"}
+}
